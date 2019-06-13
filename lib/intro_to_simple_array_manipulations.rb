2d90ef00d8b3ee05556500f02611d05d619e98ad
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-13 17:34:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@ def using_push(array, string)
   array.push(string)
 end
 
-def using_ushift(array, string)
+def using_unshift(array, string)
   array.unshift(string)
 end
 
@@ -27,11 +27,15 @@ def using_concat(array_one, array_two)
 end
 
 def using_insert(array, element)
-  array.insert(3, element)
+  array.insert(4, element)
 end
 
 def using_uniq(array)
   array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
 end
 
 def using_delete(array, string)
